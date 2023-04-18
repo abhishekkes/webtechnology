5a6e814bd066356d85bb9d7837797c1c3160bcfc
abhishekkes <abhishekkesharwani689@gmail.com>
2023-04-18 14:21:15 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function ChangeColor()
+{
+document.body.bgColor="yellow"
+}
+
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
---
+++ myscript2.js
@@ -0,0 +1,10 @@
+function myfunction()
+{
+alert("I am in external javascript")
+}
+
+function add(x,y)
+{
+var z=x+y;
+alert(z)
+}
